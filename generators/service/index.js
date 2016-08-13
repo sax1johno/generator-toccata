fe97be815d1ec2b9356c839f1ee97cdb6db85018
John O'Connor <sax1johno@gmail.com>
2016-08-13 15:14:28 -0700

    Trying to get the file to show correctly;

--- generators/service/index.js
+++ generators/service/index.js
@@ -63,7 +63,7 @@ module.exports = yeoman.Base.extend({
         "networks": {
           "app": {
             "aliases": [
-              lowerName
+              "" + lowerName
             ]
           }
         },
